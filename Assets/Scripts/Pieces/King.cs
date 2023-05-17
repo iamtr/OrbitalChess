7d40774380cbc8400ee0b58a42164d1ad267019e
tr <jettzeong@gmail.com>
2023-05-18 00:45:30 +0800

    Basic piece movement completed
    Checkmate checking, en passant and castling still require implementation.
    
    Also there is a known bug where if you click too close to a highlighted red piece the piece will not move

---
+++ Assets/Scripts/Pieces/King.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class King : Piece
+{
+	public override void GetAvailableMoves()
+	{
+		void HighlightDirection(BoardController bc, int currX, int currY, int dx, int dy, int maxDistance)
+		{
+			for (int i = 1; i <= maxDistance; i++)
+			{
+				int x = currX + i * dx;
+				int y = currY + i * dy;
+				int pos = y * 8 + x;
+				if (!IsLegalMove(x, y, this)) break;
+				bc.Highlight(x, y, this);
+				if (bc.IsOccupied(pos) && !bc.IsSamePlayer(this.CurrPos, pos)) break;
+			}
+		}
+
+		HighlightDirection(bc, currX, currY, 1, 1, 1);
+		HighlightDirection(bc, currX, currY, -1, 1, 1);
+		HighlightDirection(bc, currX, currY, 1, -1, 1);
+		HighlightDirection(bc, currX, currY, -1, -1, 1);
+		HighlightDirection(bc, currX, currY, 1, 0, 1); // Right
+		HighlightDirection(bc, currX, currY, -1, 0, 1); // Left
+		HighlightDirection(bc, currX, currY, 0, 1, 1); // Up
+		HighlightDirection(bc, currX, currY, 0, -1, 1); // Down
+	}
+
+	public override bool IsLegalMove(int x, int y, Piece p)
+	{
+		int pos = y * 8 + x;
+		if (!bc.IsInBounds(x, y) || bc.IsSamePlayer(this.CurrPos, pos))
+		{
+			return false;
+		}
+
+		return true;
+	}
+}

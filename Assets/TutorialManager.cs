a775e1a0ec34b336284f82e4642a1b54cb0e6b2b
Hong Jun <zappy.tan1234@gmail.com>
2023-06-03 03:27:01 +0800

    Storing files
    All files are turned to array of strings, with each index containing a line from the file. These are then stored in another array

--- Assets/TutorialManager.cs
+++ Assets/TutorialManager.cs
@@ -1,16 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TutorialManager : MonoBehaviour
 {
     [SerializeField] private Piece[] movementLoadout;
-    [SerializeField] private TextAsset introductionFile;
+
+    [SerializeField] private TMP_Text tutorialText;
+
+    [SerializeField] private TextAsset introFile;
     [SerializeField] private TextAsset pieceMovementFile;
 
+    private string[][] Lines;
+
+    private int FileIndex = 0;
+    private int LineIndex = 0;
+
+    private bool loadTextOnce = true;
+    
     private void Start()
     {
-        print(introductionFile);
+        ReadAndStoreFiles(introFile, pieceMovementFile);
+    }
+
+    private void Update()
+    {
+        if (!loadTextOnce) return;
+        tutorialText.text = Lines[FileIndex][LineIndex];
+        loadTextOnce = false;
+    }
+
+    public void ReadAndStoreFiles(params TextAsset[] files)
+    {
+        Lines = new string[files.Length][];
+        var splitFile = new string[] { "\r\n", "\r", "\n" };
+        int index = 0;
+        foreach (TextAsset file in files)
+        {
+            Lines[index] = file.text.Split(splitFile, System.StringSplitOptions.RemoveEmptyEntries);
+            index++;
+        }
     }
 
     public void LoadoutPieces(Piece[] pieces)

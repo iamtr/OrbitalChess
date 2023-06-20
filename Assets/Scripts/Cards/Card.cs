ac144879289e97fc2ad76c54a127124f4135b511
tr <jettzeong@gmail.com>
2023-06-20 13:39:19 +0800

    Card enlarges when hovered

--- Assets/Scripts/Cards/Card.cs
+++ Assets/Scripts/Cards/Card.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public abstract class Card : MonoBehaviour
 {
-	public abstract void Trigger(); 
+	public abstract void Trigger();
+
+	public void OnMouseOver()
+	{
+		transform.localScale = new Vector3(1, 1, 0);
+	}
+
+	public void OnMouseExit()
+	{
+		transform.localScale = new Vector3(0.5f, 0.5f, 0);
+	}
 }

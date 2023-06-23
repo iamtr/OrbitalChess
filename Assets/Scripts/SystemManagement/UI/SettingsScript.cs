6c7c90d6175ebc8de6666afe6c030923d988e60a
Hong Jun <zappy.tan1234@gmail.com>
2023-06-23 18:45:58 +0800

    Reorganize textslider ui into uimanager

--- Assets/Scripts/SystemManagement/UI/SettingsScript.cs
+++ Assets/Scripts/SystemManagement/UI/SettingsScript.cs
@@ -1,9 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class SettingsScript : MonoBehaviour
 {
 	public GameObject mainMenuPanel;
 	public GameObject settingsPanel;
+
+	public TMP_Text volumeNumber;
+	public Slider slider;
+
+	public void SetVolumeNumber()
+	{
+		volumeNumber.text = slider.value.ToString();
+	}
 
 	public void ChangeFromMainMenuToSettings()
 	{

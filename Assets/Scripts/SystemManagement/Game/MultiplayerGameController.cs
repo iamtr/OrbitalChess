773f69fd57ed86f7027d4c91cce56e951ec11de5
tr <jettzeong@gmail.com>
2023-07-06 12:24:23 +0800

    Added RoomManager script

--- Assets/Scripts/SystemManagement/Game/MultiplayerGameController.cs
+++ Assets/Scripts/SystemManagement/Game/MultiplayerGameController.cs
@@ -25,8 +25,6 @@ public class MultiplayerGameController : GameController
 		base.Start();
 		pv = GetComponent<PhotonView>();
 		playerManager = FindObjectOfType<PlayerManager>();
-
-		// StartGame();
 	}
 
 	public override void SetPlayer()
@@ -37,14 +35,6 @@ public class MultiplayerGameController : GameController
 	public override void HandleCheckAndCheckmate()
 	{
 		pv.RPC(nameof(RPC_HandleCheckAndCheckmate), RpcTarget.All);
-	}
-
-	public void OnPlayerColorSelected(int team)
-	{
-		Player player = PhotonNetwork.CurrentRoom.GetPlayer(PhotonNetwork.LocalPlayer.ActorNumber);
-		// Broadcast the player's color choice to all other players
-		pv.RPC(nameof(SyncPlayerColorChoice), RpcTarget.All, player.ActorNumber, team);
-		playerSelectionPanel.SetActive(false);	
 	}
 
 	public void StartGame()
@@ -62,34 +52,6 @@ public class MultiplayerGameController : GameController
 
 		bc.InstantiatePieces();
 		turnText.gameObject.SetActive(true);
-	}
-
-	[PunRPC]
-	private void SyncPlayerColorChoice(int playerActorNumber, int selectedTeam)
-	{
-		// Update the color choice for the corresponding player
-		Player player = PhotonNetwork.CurrentRoom.GetPlayer(playerActorNumber);
-		player.CustomProperties["PlayerType"] = selectedTeam;
-
-		if (selectedTeam == 0)
-		{
-			Debug.Log("Black selected");
-			isBlackSelected = true;
-			blackButton.interactable = false;
-		} 
-		else if (selectedTeam == 1)
-		{
-			Debug.Log("White selected");
-			isWhiteSelected = true;
-			whiteButton.interactable = false;
-		}
-
-		// Check if both players have made their selections
-		if (isBlackSelected && isWhiteSelected && !isGameStarted)
-		{
-			StartGame();
-			isGameStarted = true;
-		}
 	}
 
 	[PunRPC]

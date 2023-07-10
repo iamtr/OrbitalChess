562357ac7324e69e5b452dda96a1d2e83bb763e7
tr <jettzeong@gmail.com>
2023-07-10 21:21:19 +0800

    Rename StealCard to BurgleCard

--- Assets/Scripts/Cards/BurgleCard.cs
+++ Assets/Scripts/Cards/BurgleCard.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StealCard : Card
+public class BurgleCard : Card
 {
 	public override void Trigger()
 	{
